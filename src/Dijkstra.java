382935befe106a13dc2d38a2ac7fad8bfb5ea32c
DilkiRaveesha <dilkiraveesha99@gmail.com>
2026-04-16 11:36:21 +0530

    Added Dijkstra routing logic

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,3 +1,55 @@
+
+import java.util.*;
+
 public class Dijkstra {
+
     
+    static class Node implements Comparable<Node> {
+        String name;
+        int distance;
+
+        Node(String name, int distance) {
+            this.name = name;
+            this.distance = distance;
+        }
+
+        @Override
+        public int compareTo(Node other) {
+            return Integer.compare(this.distance, other.distance);
+        }
+    }
+
+    public static Map<String, Integer> findShortestPaths(Graph graphObj, String startNode) {
+        Map<String, Map<String, Integer>> adjacencyList = graphObj.getGraph();
+        Map<String, Integer> distances = new HashMap<>();
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
+        
+        for (String node : adjacencyList.keySet()) {
+            distances.put(node, Integer.MAX_VALUE);
+        }
+        distances.put(startNode, 0);
+        pq.add(new Node(startNode, 0));
+
+        while (!pq.isEmpty()) {
+            Node current = pq.poll();
+            String u = current.name;
+
+           
+            if (adjacencyList.get(u) != null) {
+                for (Map.Entry<String, Integer> neighbor : adjacencyList.get(u).entrySet()) {
+                    String v = neighbor.getKey();
+                    int weight = neighbor.getValue();
+                    int newDist = distances.get(u) + weight;
+
+                    // If a shorter path is found, update the record
+                    if (newDist < distances.get(v)) {
+                        distances.put(v, newDist);
+                        pq.add(new Node(v, newDist));
+                    }
+                }
+            }
+        }
+        return distances;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,38 @@
+import java.util.*;
+
+
 public class Main {
     public static void main(String[] args) {
+      
         Graph g = new Graph();
 
         g.addEdge("A", "B", 4);
         g.addEdge("A", "C", 6);
         g.addEdge("A", "D", 7);
-
         g.addEdge("B", "C", 2);
         g.addEdge("B", "E", 5);
-
         g.addEdge("C", "D", 3);
         g.addEdge("C", "E", 4);
-
         g.addEdge("D", "F", 2);
         g.addEdge("E", "G", 3);
 
+        System.out.println("--- Hospital & Blood Bank Network ---");
         g.display();
+
+       
+        String startPoint = "A";
+        System.out.println("\n--- Calculating Shortest Paths from " + startPoint + " ---");
+        
+    
+        Map<String, Integer> shortestPaths = Dijkstra.findShortestPaths(g, startPoint);
+
+  
+        System.out.println("Destination | Travel Time (Minutes)");
+        System.out.println("-----------------------------------");
+        for (String destination : shortestPaths.keySet()) {
+            int time = shortestPaths.get(destination);
+            String timeDisplay = (time == Integer.MAX_VALUE) ? "Unreachable" : time + " mins";
+            System.out.println("Location " + destination + " : " + timeDisplay);
+        }
     }
 }
